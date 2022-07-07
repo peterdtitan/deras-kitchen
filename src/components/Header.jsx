a98817134ead4052c4217fc29c7f779abbb2cd19
Peter D Titan <peterdtitan@Peters-MacBook-Pro.local>
2022-07-07 10:45:40 +0300

    Header component start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Logo from './img/logo.png';
+import { MdShoppingBasket } from 'react-icons/md';
 
 const Header = () => {
   return (
-    <div className='fixed z-50 w-screen bg-slate-400 p-6 px-16'>
+    <header className="fixed z-50 w-screen p-6 px-16">
         {/* for desktop and tablets*/}
-        <div className="hidden md:flex w-full bg-red-600 p-4"></div>
+        <div className="hidden md:flex w-full h-full">
+            <div className="flex items-center gap-2">
+                <img src={Logo} className="w-8 object-cover" alt="logo" />
+                <p className="text-headingColor text-x1 font-bold">Dera's</p>
+            </div>
+
+            <ul className="flex items-center gap-8 ml-auto" >
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Menu</li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">About Us</li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Service</li>
+            </ul>
+
+            <div className="relative">
+                <MdShoppingBasket />
+            </div>
+        </div>
 
 
         {/* for mobile */}
-        <div className="flex md:hidden w-full bg-blue-500 p-4"></div>
-    </div>
+        <div className="flex md:hidden w-full h-full"></div>
+    </header>
   )
 }
 
