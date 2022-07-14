6bc4a470f403b8ceaa32417e0f071c45fda8436c
Peter Okorafor <peterdtitan@gmail.com>
2022-07-14 11:59:17 +0300

    Fix errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold"> City</p>
+          <p className="text-headingColor text-xl font-bold"> Dera's</p>
         </Link>
 
         <div className="flex items-center gap-8">
@@ -110,7 +110,7 @@ const Header = () => {
                 exit={{ opacity: 0, scale: 0.6 }}
                 className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
               >
-                {user && user.email === "vetrivel.galaxy@gmail.com" && (
+                {user && user.email === "peterdtitan@gmail.com" && (
                   <Link to={"/createItem"}>
                     <p
                       className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
